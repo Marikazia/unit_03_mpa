7bd8b7c39a45616ebf7514122dbe61f65f69c145
Mariana Machohan <debrisofme9@gmail.com>
2022-08-18 06:17:09 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Category from './Category';
+import CategoryDescription from "./CategoryDescription";
+import Footer from './Footer';
+import Error from './Error';
+
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
+const navProps = {
+  nav: [
+    { "link": "/", "text": "Главная" },
+    { "link": "/about", "text": "О сайте" },
+    { "link": "/cat", "text": "Категории" },
+  ]
+}
+
+const catProps = {
+	nav: [
+		{ "link": "/notebook", "text": "Ноутбуки" },
+		{ "link": "/monitor", "text": "Мониторы" },
+		{ "link": "/cellphone", "text": "Мобильные телефоны" }
+	]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<>
+			<Router>
+				<Header  data={navProps} />
+
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />					
+					<Route exact path="/cat" element={<Category data={catProps} />} />
+					<Route path="cat/:catName" element={<CategoryDescription />} />
+					<Route path="*" element={< Error/>} />
+				</Routes>
+
+				<Footer />
+			</Router>
+		</>
   );
 }
 
